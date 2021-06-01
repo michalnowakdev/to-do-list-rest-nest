66c3ae7b6f0fa32c2681622f7243ed8aef88b170
Michał Nowak <nowakmichal9696@gmail.com>
2021-06-01 14:26:05 +0200

    Added mocked response from controller

---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,5 @@
+export class CreateTaskDto {
+  readonly id: number;
+  readonly description: string;
+  readonly completed: boolean;
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   @Get()
   findAll(): string {
     return 'This action returns all tasks';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `found one ${id}`;
+  }
+
+  @Post()
+  createNewTask(@Body() createTaskDto: CreateTaskDto): string {
+    return `Created description = ${createTaskDto.description}`;
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id): string {
+    return `Deleted ${id}`;
+  }
+
+  @Put(':id')
+  updateTask(@Body() updateTaskDto: CreateTaskDto, @Param('id') id): string {
+    return `Update ${id} - Desc: ${updateTaskDto.description}`;
   }
 }
